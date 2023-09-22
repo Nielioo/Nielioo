fc51474f217fed875eaa8d072dca625a1b02ea26
Daniel Aprillio <70996943+Nielioo@users.noreply.github.com>
2023-09-22 20:43:10 +0700

    feat: Update readme_updater.py

--- readme_updater.py
+++ readme_updater.py
@@ -16,15 +16,22 @@ with open("./README.md", "w") as f:
 [![GitHub stats-Dark](https://custom-github-readme-stats.vercel.app/api?username=nielioo&show_icons=true&theme=slateorange&bg_color=00000000&hide_title=true#gh-dark-mode-only)](https://github.com/Nielioo#gh-dark-mode-only)
 [![GitHub stats-Light](https://custom-github-readme-stats.vercel.app/api?username=nielioo&show_icons=true&theme=buefy&bg_color=00000000&hide_title=true#gh-light-mode-only)](https://github.com/Nielioo#gh-light-mode-only)
 
+</div>
+<br/>
+
+## Snake Animation
+
+<details> 
+  <summary>Show</summary>
   <picture>
     <source media="(prefers-color-scheme: dark)" srcset="https://raw.githubusercontent.com/Nielioo/Nielioo/snake-svg-animation-output/grid-snake-dark.svg" />
     <source media="(prefers-color-scheme: light)" srcset="https://raw.githubusercontent.com/Nielioo/Nielioo/snake-svg-animation-output/grid-snake-light.svg" />
     <img alt="GitHub contribution animation" src="https://raw.githubusercontent.com/Nielioo/Nielioo/snake-svg-animation-output/grid-snake.svg" />
   </picture>
-
-  <h6>
-    Updated at {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}
-  </h6>
+</details>
+<br/>
   
-</div>
+<h6>
+  Updated at {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}
+</h6>
 ''')

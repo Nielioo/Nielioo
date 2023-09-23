fd3beb6a120cdd3bd641942c2b7a364ab157348a
Daniel Aprillio <70996943+Nielioo@users.noreply.github.com>
2023-09-23 15:30:50 +0700

    feat: Update readme_updater.py

--- readme_updater.py
+++ readme_updater.py
@@ -10,6 +10,14 @@ with open("./README.md", "w") as f:
   <img src="https://github.com/Nielioo/Nielioo/blob/main/Assets/Raw/XinyanCheer.gif" width="12%"/>
   <img src="https://github.com/Nielioo/Nielioo/blob/main/Assets/Raw/XinyanGuitar.gif" width="12%"/>
 
+# Hi! It's me [Nielioo](https://www.linkedin.com/in/daniel-aprillio/), nice to meet ya!
+
+[![LinkedIn Badge](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/daniel-aprillio/)
+[![Discord Badge](https://img.shields.io/badge/Discord-7289DA?style=for-the-badge&logo=discord&logoColor=white)](https://discord.com/users/551736880014819329)
+[![Telegram Badge](https://img.shields.io/badge/Telegram-2CA5E0?style=for-the-badge&logo=telegram&logoColor=white)](https://t.me/Nielioo)
+[![Line Badge](https://img.shields.io/badge/Line-00C300?style=for-the-badge&logo=line&logoColor=white)](https://line.me/ti/p/~daniel_aprillio)
+[![YouTube Badge](https://img.shields.io/badge/YouTube-FF0000?style=for-the-badge&logo=youtube&logoColor=white)](https://youtube.com/Nielio?sub_confirmation=1)
+
 [![Top Langs-Dark](https://custom-github-readme-stats.vercel.app/api/top-langs/?username=nielioo&layout=compact&theme=slateorange&bg_color=00000000&hide=jupyter%20notebook#gh-dark-mode-only)](https://github.com/Nielioo#gh-dark-mode-only)
 [![Top Langs-Light](https://custom-github-readme-stats.vercel.app/api/top-langs/?username=nielioo&layout=compact&theme=buefy&bg_color=00000000&hide=jupyter%20notebook#gh-light-mode-only)](https://github.com/Nielioo#gh-light-mode-only)
 [![GitHub stats-Dark](https://custom-github-readme-stats.vercel.app/api?username=nielioo&show_icons=true&theme=slateorange&bg_color=00000000&hide_title=true#gh-dark-mode-only)](https://github.com/Nielioo#gh-dark-mode-only)
@@ -29,7 +37,7 @@ with open("./README.md", "w") as f:
   </picture>
 </details>
 <br/>
-
+  
 <h6>
   Updated at {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}
 </h6>

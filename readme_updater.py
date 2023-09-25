f9739a1c9092da968e2e8aa45c29cb309bae3fd8
Daniel Aprillio <70996943+Nielioo@users.noreply.github.com>
2023-09-25 17:04:18 +0700

    feat: Update readme_updater.py

--- readme_updater.py
+++ readme_updater.py
@@ -5,8 +5,8 @@ def generate():
     with open("./README.md") as f:
         readme_content = f.read()
     
-    readme_replacement = f'Updated: {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}'
-    new_readme = re.sub('Updated: .*', readme_replacement, readme_content)
+    readme_replacement = f'Updated at {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}'
+    new_readme = re.sub('Updated at .*', readme_replacement, readme_content)
 
     with open("./README.md", "w") as f:
         f.write(new_readme)
